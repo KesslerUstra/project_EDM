77f4a11899dc162f0aa9f65d1d20a66ee13726f7
Eduardo Kessler Ustra <eduardoksustra@gmail.com>
2023-07-24 21:05:35 -0300

    finalizando o esqueleto basico algoritmo genetico

--- app/functions.js
+++ app/functions.js
@@ -41,6 +41,6 @@ export function orderSliceArray(order = false, population, subArray){
     return subArrays;
 }
 
-function resultFunction(data){
+export function resultFunction(data){
     return (0.5*8*Math.pow( Math.sqrt( Math.pow(data[0], 2) + Math.pow( 10-data[1], 2) ) - 10, 2) + 0.5*1*Math.pow( Math.sqrt( Math.pow(data[0], 2) + Math.pow( 10+data[1], 2) ) - 10, 2) - 5*data[0] - 5*data[1]);
 }
--- app/page.js
+++ app/page.js
@@ -1,11 +1,10 @@
 "use client"
 
 import React, { useState } from 'react'
-
 import Input from '@/components/Input'
-import { createPopulation, orderSliceArray } from './functions'
+import { createPopulation, orderSliceArray, resultFunction } from './functions'
 import styles from './page.module.css'
-
+import { sampleSize, cloneDeep } from 'lodash'
 
 export default function Home() {
 
@@ -21,7 +20,7 @@ export default function Home() {
   }
 
   function changeDataLimits(type, idx, value){
-    setLimits(prev => ({...prev, [idx]: { ...prev[idx], [type]: parseInt(value) ? parseInt(value) : null}}));
+    setLimits(prev => ({...prev, [idx]: { ...prev[idx], [type]: parseInt(value) !== NaN ? parseInt(value) : null}}));
   }
 
   function renderItems(){
@@ -43,6 +42,120 @@ export default function Home() {
     console.log('population', newPop);
     newPop = orderSliceArray(false, newPop, data.points);
     setPopulation(newPop);
+    console.log('pop', newPop);
+    console.log('pop',betterPoint(newPop));
+    newPop = AlghoritmED(newPop);
+    console.log('pop', newPop);
+    console.log('pop',betterPoint(newPop));
+  }
+
+  function AlghoritmED(pop){
+    let k = 0, g = 0, i = 1;
+    let popChanges = cloneDeep(pop);
+    for (k = 0; k < pop.length; k++) {
+      for ( g = 0; g < data.generations; g++) {
+        let subPop = cloneDeep(popChanges[k]);
+        for (i = 0; i < pop[k].length; i++) {
+
+          //MUTAÇÃO
+
+          console.log(`teste / K = ${k} - G = ${g} - I = ${i}`);
+          let setChosen = cloneDeep(subPop[i]);
+          let arrayInicial = cloneDeep(subPop);
+          arrayInicial.splice(i, 1);
+          console.log(arrayInicial);
+          console.log(setChosen);
+
+          let teste = sampleSize(arrayInicial, 3);
+          console.log(teste); 
+          console.log(actionVector(teste[0], teste[1]));
+          let teste2 = actionVector(teste[0], actionVector(actionVector(teste[1], teste[2], 'subtracao'), [], 'multiplicacaoValor', 1.4));
+          console.log(teste2);
+
+          //CRUZAMENTO
+
+          let cr = 0.6;
+          let teste3 = crossingVector(cr, teste2, setChosen);
+
+          //Verificando viabilidade do vetor U
+
+          for(const property in teste3){
+            if( teste3[property] < limits[property].inferior_limit){
+              teste3[property] = limits[property].inferior_limit;
+            }else if(teste3[property] > limits[property].upper_limit){
+              teste3[property] = limits[property].upper_limit;
+            }
+          }
+
+          //SELEÇÃO
+
+          teste3.result = resultFunction(teste3);
+          if(setChosen.result === undefined){
+            setChosen.result = resultFunction(setChosen);
+          }
+
+          if(teste3.result < setChosen.result){
+            popChanges[k][i] = teste3;
+          }else{
+            popChanges[k][i] = setChosen;
+          }
+        }
+      }
+    }
+    return popChanges;
+  }
+
+  function actionVector(arry1, arry2, type, value = 1){
+    console.log(arry1);
+    console.log(arry2);
+    console.log(type);
+    console.log(value);
+    let arryFinal = {};
+    for(const property in arry1){
+      if(property !== 'result'){
+        switch (type) {
+          case 'soma':
+            console.log('soma');
+            arryFinal[property] = parseFloat((arry1[property] + arry2[property]).toFixed(2));
+          break;
+          case 'subtracao':
+            console.log('subtracao');
+            arryFinal[property] = parseFloat((arry1[property] - arry2[property]).toFixed(2));
+          break;
+          case 'multiplicacaoValor':
+            console.log('multiplicacaoValor');
+            arryFinal[property] = parseFloat((arry1[property] * value).toFixed(2));
+          break;
+          default:
+            console.log('error');
+            arryFinal[property] = parseFloat((arry1[property] + arry2[property]).toFixed(2));
+          break;
+        }
+      }
+    }
+    return arryFinal;
+  }
+
+  function crossingVector(cr, arryV, arryX){
+    let arryFinal = {};
+    for(const property in arryV){
+      if(property === 'result'){
+        return;
+      }
+      if(Math.random() > cr){
+        console.log('Entrou no Cruzamento', property);
+        arryFinal[property] = arryX[property];
+      }else{
+        console.log('Não Entrou no Cruzamento', property);
+        arryFinal[property] = arryV[property];
+      }
+    }
+    return arryFinal;
+  }
+
+  function betterPoint(pop){
+    let arry = pop.flat().sort((a, b) => a.result - b.result);
+    return arry;
   }
 
 

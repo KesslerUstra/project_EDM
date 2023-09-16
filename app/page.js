90885183a79cc8220a8090e7d24a90ab43b16da2
Eduardo Kessler Ustra <eduardoksustra@gmail.com>
2023-09-16 04:24:57 -0300

    adicionando responsividade

--- app/page.js
+++ app/page.js
@@ -13,9 +13,9 @@ export default function Home() {
         </div>
         <div className={styles.title_list_box}>
           <div className={styles.stroke}></div>
-          <div className={styles.bar} style={{margin: ' 0px 36px 0px -10px'}}></div>
+          <div className={`${styles.bar} ${styles.bar_margin_1}`}></div>
           <h2>Lista de Problemas</h2>
-          <div className={styles.bar} style={{margin: ' 0px 20px 0px 6px'}}></div>
+          <div className={`${styles.bar} ${styles.bar_margin_2}`}></div>
           <div className={styles.stroke}></div>
         </div>
         <SearchProblems />
